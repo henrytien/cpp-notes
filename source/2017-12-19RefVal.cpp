c405c0330bf104fe2e7a2db3f2a3bacbcf28c308
henrytien <tianhangyu8886@gmail.com>
2018-09-29 18:23:33 +0800

    delete source directory

--- source/2017-12-19RefVal.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-int main()
-{
-    int null = 0;
-    int *p = &null;
-    std::cout<<"*p:"<<*p<<std::endl;
-    return 0;
-}
--- source/2018-2-6githubcmd.cpp
+++
@@ -1,4 +0,0 @@
-
-stars:10000..20000 size:>1000
-
-forks:>=10000  language:c
